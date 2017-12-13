87388ba89e828d8f2374fc0d31347e12ff8883b6
Michelle Pinsky <michellepinsky@unboxedtechnology.com>
2017-12-12 19:04:29 -0500

    Adding API keys

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,9 @@
 // Initialize Variables Below Here //
-
+var yelpKey = "i-A7nErw7MDEJwRipAmxYQ9VBBOt6lDWxiDNa6khg5f038W7y8S7qgqnezv4J2e5l0KtHWsWDZEJXqxhNrwkkuw2AAabs_ssS5z9Dt72AAtG9D9U8-iSqUaI1jksWnYx"
+var eventKey = "7KPSIMHAIA7KBMEV6X"
+var youtubeKey = "AIzaSyB6LbPrt3WHmEgdLBQUCa3DrYe7gsTEqSk"
+var mapsKey = "AIzaSyCtXevtm_ndCcVgCNJPosfToHZWVQ-ct78"
+var pixabayKey = "7371572-b4d7f234c51422f2be6d8c9f2"
 // Logic Below Here //
 $(document).ready(function() {
 
