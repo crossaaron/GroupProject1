c709e157935f9403b637f9a1816a40a6836cdd5f
zrcarter2017 <zcarter@allegiancy.us>
2017-12-12 21:34:21 -0500

    picture api and google maps api

--- js/logic.js
+++ js/logic.js
@@ -4,8 +4,46 @@ var eventKey = "7KPSIMHAIA7KBMEV6X"
 var youtubeKey = "AIzaSyB6LbPrt3WHmEgdLBQUCa3DrYe7gsTEqSk"
 var mapsKey = "AIzaSyCtXevtm_ndCcVgCNJPosfToHZWVQ-ct78"
 var pixabayKey = "7371572-b4d7f234c51422f2be6d8c9f2"
+
+// var API_KEY = '7371572-b4d7f234c51422f2be6d8c9f2';
+// var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
+// $.getJSON(URL, function(data){
+// if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
+// else
+//     console.log('No hits');
+// });
+
+
+
+
+
+
+
+     $.ajax({
+        url: "https://pixabay.com/api/?key="+pixabayKey+"&q="+encodeURIComponent('san diego'),
+        method: 'GET'
+ 	 }).done(
+            function (response){
+            	for (var i = 0; i < 4; i++) {
+            		// $('#pictures').attr("src", response.hits[0].webformatURL);
+            		$('.pictures').prepend("<img class = 'searchImages' src='" + response.hits[i].webformatURL + "'>");
+            	};
+
+                console.log(response.hits[0].webformatURL);
+            }
+        );
+
+
+
+
+
+
 // Logic Below Here //
 $(document).ready(function() {
+
+
+    
 
 })
 
