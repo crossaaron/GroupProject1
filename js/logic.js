a49dd48f03212b440024d87b81e91a3ea4ea2e7c
Michelle Pinsky <michellepinsky@unboxedtechnology.com>
2017-12-12 19:56:44 -0500

    JS change for eventbrite

--- js/logic.js
+++ js/logic.js
@@ -1,17 +1,21 @@
 // Initialize Variables Below Here //
 
-
 var interest = ""
 var location = ""
 var distance = ""
 
+// AJAX Calls //
 $.ajax({
-    url: "https://www.eventbriteapi.com/v3/events/search?q="+interest+"&?q="+location+"&?q="+distance,
+    url: "https://www.eventbrite.com/oauth/authorize?response_type=token&client_id=7KPSIMHAIA7KBMEV6X"
     method: "GET"
 }).done(
     function (response){
-        $(".city").text(response.city.name);
-        console.log(response);
+        $.ajax({
+            url: "https://www.eventbriteapi.com/v3/users/me/?token=PGYDBOPFSVG2QZQ64KDP/events/search"
+            method: "GET"
+        }).done(
+            function(results)
+        );
     }
 );
 // Logic Below Here //
