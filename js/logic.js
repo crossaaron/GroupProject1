c1afd9c28e328acc5e8e37d14e543d892120c94b
Michelle Pinsky <michellepinsky@unboxedtechnology.com>
2017-12-12 19:32:59 -0500

    Updating javascript

--- js/logic.js
+++ js/logic.js
@@ -1,9 +1,19 @@
 // Initialize Variables Below Here //
-var yelpKey = "i-A7nErw7MDEJwRipAmxYQ9VBBOt6lDWxiDNa6khg5f038W7y8S7qgqnezv4J2e5l0KtHWsWDZEJXqxhNrwkkuw2AAabs_ssS5z9Dt72AAtG9D9U8-iSqUaI1jksWnYx"
-var eventKey = "7KPSIMHAIA7KBMEV6X"
-var youtubeKey = "AIzaSyB6LbPrt3WHmEgdLBQUCa3DrYe7gsTEqSk"
-var mapsKey = "AIzaSyCtXevtm_ndCcVgCNJPosfToHZWVQ-ct78"
-var pixabayKey = "7371572-b4d7f234c51422f2be6d8c9f2"
+
+
+var interest = ""
+var location = ""
+var distance = ""
+
+$.ajax({
+    url: "https://www.eventbriteapi.com/v3/events/search?q="+interest+"&?q="+location+"&?q="+distance,
+    method: "GET"
+}).done(
+    function (response){
+        $(".city").text(response.city.name);
+        console.log(response);
+    }
+);
 // Logic Below Here //
 $(document).ready(function() {
 
