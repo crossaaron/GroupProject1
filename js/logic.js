261c27c209b6eabf549d1da09adb278d211c70ee
Aaron Cross <aaroncross@Aarons-MacBook-Pro.local>
2017-12-14 21:36:59 -0500

    create 1 html

--- js/logic.js
+++ js/logic.js
@@ -31,12 +31,43 @@ $.ajax({
     }
 );
 
+//Google Places
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.866, lng: 151.196},
+          zoom: 15
+        });
 
+        var infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+
+        service.getDetails({
+          placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
+        }, function(place, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            var marker = new google.maps.Marker({
+              map: map,
+              position: place.geometry.location
+            });
+            google.maps.event.addListener(marker, 'click', function() {
+              infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                'Place ID: ' + place.place_id + '<br>' +
+                place.formatted_address + '</div>');
+              infowindow.open(map, this);
+            });
+          }
+        });
+      }
+    
 
 
 // Logic Below Here //
 $(document).ready(function() {
 
+$("getResults").on('click', function() {
+    
+
+});
 
     	
 
@@ -44,15 +75,5 @@ $(document).ready(function() {
 
 // Set Functions Below Here //
 
-// Google maps
-function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-}
+
+
