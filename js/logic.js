e309f19bc6a5f06183be17da5b043e38c9bf5cad
Aaron Cross <aaroncross@Aarons-MacBook-Pro.local>
2017-12-19 18:30:03 -0500

    michelle html edits

--- js/logic.js
+++ js/logic.js
@@ -53,7 +53,7 @@
               position: place.geometry.location
             });
             // RATING CALL just need DIV?
-            $(place.rating);
+            console.log(place.rating);
 
             google.maps.event.addListener(marker, 'click', function() {
               infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
@@ -139,7 +139,7 @@ function hitSubmit() {
         const sfEvents = events.filter(function(event){
 
                 
-        $("#eventBox").prepend('<div class="card listEntry"><div class="card-header"> <div class="row"> <div class="col-md-3" id="name">' + event.name.text + '</div> <div class="col-md-3" id="price">' + '<a target="_blank" href="' + event.url + '">Tickets/Pricing</a></div> <div class="col-md-3" id="location">' +event.venue.address.city + '</div> <div class="col-md-3" id="date">' + event.end.utc + '</div> </div> </div> <div class="card-body"> <div class="row"><div class=col-md-8><p class="card-text" id="eventDescription">' + event.description.text + '</p></div><div class="col-md-4 googlemaps"><div id="map"></div></div>');
+        $("#eventBox").prepend('<div class="card listEntry"><div class="card-header"> <div class="row"> <div class="col-md-3" id="name">' + event.name.text + '</div> <div class="col-md-3" id="price">' + '<a target="_blank" href="' + event.url + '">Tickets/Pricing</a></div> <div class="col-md-3" id="location">' +event.venue.address.city + '</div> <div class="col-md-3" id="date">' + event.end.utc + '</div> </div> </div> <div class="card-body"> <div class="row"><div class=col-md-12><p class="card-text" id="eventDescription">' + event.description.text + '</p></div><div class="col-md-12 googlemaps"><div id="map"></div></div>');
         
             initMap(parseFloat(event.venue.address.latitude), parseFloat(event.venue.address.longitude));
                
