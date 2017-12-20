16f21e1b46151dd1f0e1407c5515964d3e0534fd
Aaron Cross <aaroncross@Aarons-MBP.fios-router.home>
2017-12-20 09:53:41 -0500

    firebase storage added for pictures :

--- js/logic.js
+++ js/logic.js
@@ -8,26 +8,15 @@
     storageBucket: "classdemo-743ef.appspot.com",
     messagingSenderId: "109836927180"
   };
-  
-
-    firebase.initializeApp(config);
-
-    var database = firebase.database();  
+  firebase.initializeApp(config);
     
-// Pixabay
-// var callPictures = function(interestInput) {
+    var database = firebase.database(); 
+    var storage = firebase.storage();
+    var storageRef = storage.ref();
+    var gsReference = storage.refFromURL('gs://classdemo-743ef.appspot.com/conImages/battlestar.jpeg')
+    var count = 0
 
-// $.ajax({
-//     url: "https://pixabay.com/api/?key="+"7371572-b4d7f234c51422f2be6d8c9f2"+"&q="+encodeURIComponent(interestInput),
-//     method: 'GET'
-// }).done(function (response){
-//     for (var i = 0; i < 4; i++) {
-//     $('.pictures').prepend("<img class = 'searchImages' src='" + response.hits[i].webformatURL + "'>");
-//     };
-//     console.log(response.hits[0].webformatURL);
-//     }
-// );
-// };
+
 
 //Google Places
     var initMap = function(latitude, longitude) {
@@ -36,6 +25,7 @@
           center: uluru,
           zoom: 15
         });
+
         var marker = new google.maps.Marker({
             position: uluru ,
             map: map
@@ -45,37 +35,41 @@
         var service = new google.maps.places.PlacesService(map);
 
         service.getDetails({
-          placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
-        }, function(place, status) {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-            var marker = new google.maps.Marker({
-              map: map,
-              position: place.geometry.location
-            });
-            // RATING CALL just need DIV?
-            $(place.rating);
+          placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'},
+        
+            function(place, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    var marker = new google.maps.Marker({
+                    map: map,
+                    position: place.geometry.location
+                    });
 
-            google.maps.event.addListener(marker, 'click', function() {
-              infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-                'Place ID: ' + place.place_id + '<br>' +
-                place.formatted_address + '</div>');
-              infowindow.open(map, this);
-            });
-          }
-        });
-      } 
+                    google.maps.event.addListener(marker, 'click', function() {
+                        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+                        'Place ID: ' + place.place_id + '<br>' +
+                        place.formatted_address + '</div>');
+                        infowindow.open(map, this);
+
+                    });
+                }
+            }); 
+    } 
 
 // Logic Below Here //
 $(document).ready(function() {
 
+    $(function(){
+        var $select = $("#searchRadius");
+        for (i=10;i<=100;i=i+10){
+            $select.append($('<option></option>').val(i).html(i))
+        }
+    });
 
-
-
+    
     $("#getResults").on("click", function(event) {
       event.preventDefault();
 
-      var interest = $('#interest').val().trim();
-        // callPictures(interest);
+        var interest = $('#interest').val().trim();
         hitSubmit();
 
         var searchName;
@@ -87,23 +81,19 @@ $(document).ready(function() {
         var searchInterest;
         if ($("#interest").val()) searchInterest = $("#interest").val().trim();
 
-
         // Create Database object
-
         var newInput = {
             name: searchName || "",
             location: searchLocation,
             radius: searchRadius,
             interest: searchInterest, 
         };
-        database.ref().push(newInput);       
-});
+        database.ref().push(newInput);
+
+    });
       
 })
-    
-
 // Set Functions Below Here //
-
 
 // Eventbrite
 function hitSubmit() {
@@ -130,18 +120,29 @@ function hitSubmit() {
     $.ajax(conSettings).done(function(eventObject){
                 
         const events = eventObject.events;
-        const sfEvents = events.filter(function(event){
+        const getEvents = events.filter(function(event){
+
                 
-        $("#eventBox").prepend('<div class="card listEntry"><div class="card-header"> <div class="row"> <div class="col-md-3" id="name">' + event.name.text + '</div> <div class="col-md-3" id="price">' + '<a target="_blank" href="' + event.url + '">Tickets/Pricing</a></div> <div class="col-md-3" id="location">' +event.venue.address.city + '</div> <div class="col-md-3" id="date">' + event.end.utc + '</div> </div> </div> <div class="card-body"> <div class="row"><div class=col-md-8><p class="card-text" id="eventDescription">' + event.description.text + '</p></div><div class="col-md-4 googlemaps"><div id="map"></div></div>');
-        
-        
+        $("#eventBox").prepend('<div class="card listEntry"><div class="card-header"> <div class="row"> <div class="col-md-4" id="name">' + event.name.text + '</div> <div class="col-md-2" id="price">' + '<a target="_blank" href="' + event.url + '">Tickets/Pricing</a></div> <div class="col-md-2" id="location">' +event.venue.address.city + '</div> <div class="col-md-2" id="rating"> </div> <div class="col-md-2" id="date">' + event.end.utc + '</div> </div> </div> <div class="card-body"> <div class="row"><div class=col-md-12><p class="card-text" id="eventDescription">' + event.description.text + '</p></div><div class="col-md-12 googlemaps"><div id="map"></div></div><div class="row"><div class="col-sm-12"><img class="col-sm-3" id="myimg0"><img class="col-sm-3" id="myimg1"><img class="col-sm-3" id="myimg2"><img class="col-sm-2" id="myimg3"></div></div>');
+
             initMap(parseFloat(event.venue.address.latitude), parseFloat(event.venue.address.longitude));
-               
+            getPics();
 
-            return event.venue.address.city.toLowerCase() === $("#location").val().toLowerCase();
-
-            
         });
-    console.log(sfEvents.length);    
     });
 }
+
+// pull pictures from firebase storage
+    function getPics() {
+        for (var i = 0; i < 4; i++) {
+            picNumb = Math.floor(Math.random() * 24)
+
+        storageRef.child('conImages/pic'+ picNumb + '.jpeg').getDownloadURL().then(function(url){
+
+        var img = document.getElementById('myimg'+ count);
+        img.src = url;
+        count++
+        });
+        };
+    };
+
