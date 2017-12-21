52723e90a859e5aa52a2231c1452d9720ae230ae
Aaron Cross <aaroncross@Aarons-MBP.fios-router.home>
2017-12-20 20:28:34 -0500

    fixed date formatting error.  had unneeded div in there

--- js/logic.js
+++ js/logic.js
@@ -131,7 +131,7 @@ function hitSubmit() {
 
                 
 
-        $("#eventBox").prepend('<div class="card listEntry"><div class="card-header"> <div class="row"> <div class="col-md-4" id="name">' + event.name.text + '</div> <div class="col-md-2" id="price">' + '<a target="_blank" href="' + event.url + '">Tickets/Pricing</a></div> <div class="col-md-2" id="location">' +event.venue.address.city + '</div> <div class="col-md-2" id="rating"> </div> <div class="col-md-2" id="date">' + event.end.utc + '</div> </div> </div> <div class="card-body"> <div class="row"><div class=col-md-12><p class="card-text" id="eventDescription">' + event.description.text + '</p></div><div class="row"><div class="col-sm-12"><img class="col-sm-3" id="myimg0"><img class="col-sm-3" id="myimg1"><img class="col-sm-3" id="myimg2"><img class="col-sm-2" id="myimg3"></div></div><div class="col-md-12 googlemaps"><div id="map"></div></div>');
+        $("#eventBox").prepend('<div class="card listEntry"><div class="card-header"> <div class="row"> <div class="col-md-5" id="name">' + event.name.text + '</div> <div class="col-md-2" id="price">' + '<a target="_blank" href="' + event.url + '">Tickets/Pricing</a></div> <div class="col-md-2" id="location">' +event.venue.address.city + '</div><div class="col-md-2" id="date">' + event.end.utc + '</div> </div> </div> <div class="card-body"> <div class="row"><div class=col-md-12><p class="card-text" id="eventDescription">' + event.description.text + '</p></div><div class="row"><div class="col-sm-12"><img class="col-sm-3" id="myimg0"><img class="col-sm-3" id="myimg1"><img class="col-sm-3" id="myimg2"><img class="col-sm-2" id="myimg3"></div></div><div class="col-md-12 googlemaps"><div id="map"></div></div>');
 
 
             initMap(parseFloat(event.venue.address.latitude), parseFloat(event.venue.address.longitude));
@@ -154,4 +154,5 @@ function hitSubmit() {
         });
         };
     };
+});
 
